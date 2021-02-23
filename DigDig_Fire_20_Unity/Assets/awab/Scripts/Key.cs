4b8bff1dc2919d2bfb67a70ca397f74c732edf2f
Awabb <74252895+Awabb@users.noreply.github.com>
2021-02-23 16:05:21 +0100

    Key test

---
+++ DigDig_Fire_20_Unity/Assets/awab/Scripts/Key.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+    [SerializeField] private KeyType keyType; 
+    public enum KeyType {
+        Yellow
+    }
+
+    public KeyType GetKeyType() {
+        return keyType;
+
+    }
+}
